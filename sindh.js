3af3547acfdc0f91d1c2a27e9c22dc95e6519732
khadijaasif2300 <khadijaasif230@gmail.com>
2025-08-26 23:55:49 +0500

    sindh descriptions

--- sindh.js
+++ sindh.js
@@ -1,7 +1,65 @@
 $(document).ready(function(){
-    $(".footer").load("footer.html");
-});
-
-$(document).ready(function(){
     $(".header").load("header.html");
+
+    $(".footer").load("footer.html");
+
+    $("#Mohenjo-Daro").click(function () {
+        window.location.href = "sindh-description/Mohenjo-Daro.html";
+    });
+
+    $("#Makli Necropolis").click(function () {
+        window.location.href = "sindh-description/Makli Necropolis.html";
+    });
+
+    $("#Shah Jahan Mosque").click(function () {
+        window.location.href = "sindh-description/Shah Jahan Mosque.html";
+    });
+
+    $("#Ranikot Fort").click(function () {
+        window.location.href = "sindh-description/Ranikot Fort.html";
+    });
+
+    $("#Kot Diji Fort").click(function () {
+        window.location.href = "sindh-description/Kot Diji Fort.html";
+    });
+
+    $("#Tomb of shah").click(function () {
+        window.location.href = "sindh-description/Tomb of shah.html";
+    });
+
+    $("#Tomb of Lal").click(function () {
+        window.location.href = "sindh-description/Tomb of Lal.html";
+    });
+
+    $("#Chaukhandi Tombs").click(function () {
+        window.location.href = "sindh-description/Chaukhandi Tombs.html";
+    });
+
+    $("#Frere Hall").click(function () {
+        window.location.href = "sindh-description/Frere Hall.html";
+    });
+
+    $("#Keenjhar Lake").click(function () {
+        window.location.href = "sindh-description/Keenjhar Lake.html";
+    });
+
+    $("#Haleji Lake").click(function () {
+        window.location.href = "sindh-description/Haleji Lake.html";
+    });
+
+     $("#Gorakh Hill Station").click(function () {
+        window.location.href = "sindh-description/Gorakh Hill Station.html";
+    });
+
+     $("#Kirthar National Park").click(function () {
+        window.location.href = "sindh-description/Kirthar National Park.html";
+    });
+
+     $("#Clifton Beach").click(function () {
+        window.location.href = "sindh-description/Clifton Beach.html";
+    });
+
+     $("#Hawksbay Beach").click(function () {
+        window.location.href = "sindh-description/Hawksbay Beach.html";
+    });
 });
