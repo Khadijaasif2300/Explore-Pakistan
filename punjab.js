90aeed29f4b9316aeb88539e6ff19f17cec825e9
khadijaasif2300 <khadijaasif230@gmail.com>
2025-08-26 18:33:07 +0500

    added

--- punjab.js
+++ punjab.js
@@ -13,4 +13,56 @@ $(document).ready(function () {
     $(".tomb").click(function () {
         window.location.href = "punjab-descriptions/tomb.html";
     });
+
+    $(".rohat-fort").click(function () {
+        window.location.href = "punjab-descriptions/rohat-fort.html";
+    });
+
+    $(".derawarr-fort").click(function () {
+        window.location.href = "punjab-descriptions/derawarr-fort.html";
+    });
+
+    $(".noor-mahal").click(function () {
+        window.location.href = "punjab-descriptions/noor-mahal.html";
+    });
+
+    $(".hiran-minar").click(function () {
+        window.location.href = "punjab-descriptions/hiran-minar.html";
+    });
+
+    $(".clock-tower").click(function () {
+        window.location.href = "punjab-descriptions/clock-tower.html";
+    });
+
+    $(".raj-temple").click(function () {
+        window.location.href = "punjab-descriptions/raj-temple.html";
+    });
+
+    $(".shah-tomb").click(function () {
+        window.location.href = "punjab-descriptions/shah-tomb.html";
+    });
+
+    $(".shah-sultan").click(function () {
+        window.location.href = "punjab-descriptions/shah-sultan.html";
+    });
+
+    $(".murree-hills").click(function () {
+        window.location.href = "punjab-descriptions/murree-hills.html";
+    });
+
+     $(".khewra").click(function () {
+        window.location.href = "punjab-descriptions/khewra.html";
+    });
+
+     $(".msoon-vally").click(function () {
+        window.location.href = "punjab-descriptions/soon-vally.html";
+    });
+
+     $(".cholistan").click(function () {
+        window.location.href = "punjab-descriptions/cholistan.html";
+    });
+
+     $(".lahore-museum").click(function () {
+        window.location.href = "punjab-descriptions/lahore-museum.html";
+    });
 });
