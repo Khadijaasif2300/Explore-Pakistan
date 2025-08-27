87c6990b5d122109172218642bef93059f3bfd6a
khadijaasif2300 <khadijaasif230@gmail.com>
2025-08-27 22:03:58 +0500

    more changes

--- sindh.js
+++ sindh.js
@@ -7,63 +7,63 @@ $(document).ready(function(){
         window.location.href = "sindh-description/Mohenjo-Daro.html";
     });
 
-    $("#Makli Necropolis").click(function () {
+    $("#Makli-Necropolis").click(function () {
         window.location.href = "sindh-description/Makli-Necropolis.html";
     });
 
-    $("#Shah Jahan Mosque").click(function () {
+    $("#Shah-Jahan-Mosque").click(function () {
         window.location.href = "sindh-description/Shah-Jahan-Mosque.html";
     });
 
-    $("#Ranikot Fort").click(function () {
+    $("#Ranikot-Fort").click(function () {
         window.location.href = "sindh-description/Ranikot-Fort.html";
     });
 
-    $("#Kot Diji Fort").click(function () {
+    $("#Kot-Diji-Fort").click(function () {
         window.location.href = "sindh-description/Kot-Diji-Fort.html";
     });
 
-    $("#Tomb of shah").click(function () {
+    $("#Tomb-of-shah").click(function () {
         window.location.href = "sindh-description/Tomb-of-shah.html";
     });
 
-    $("#Tomb of Lal").click(function () {
+    $("#Tomb-of-Lal").click(function () {
         window.location.href = "sindh-description/Tomb-of-Lal.html";
     });
 
-    $("#Chaukhandi Tombs").click(function () {
+    $("#Chaukhandi-Tombs").click(function () {
         window.location.href = "sindh-description/Chaukhandi-Tombs.html";
     });
 
-    $("#Frere Hall").click(function () {
+    $("#Frere-Hall").click(function () {
         window.location.href = "sindh-description/Frere-Hall.html";
     });
 
-    $("#Keenjhar Lake").click(function () {
+    $("#Keenjhar-Lake").click(function () {
         window.location.href = "sindh-description/Keenjhar-Lake.html";
     });
 
-    $("#Haleji Lake").click(function () {
+    $("#Haleji-Lake").click(function () {
         window.location.href = "sindh-description/Haleji-Lake.html";
     });
 
-     $("#Gorakh Hill Station").click(function () {
+     $("#Gorakh-Hill-Station").click(function () {
         window.location.href = "sindh-description/Gorakh-Hill-Station.html";
     });
 
-     $("#Kirthar National Park").click(function () {
+     $("#Kirthar-National-Park").click(function () {
         window.location.href = "sindh-description/Kirthar-National-Park.html";
     });
 
-     $("#Clifton Beach").click(function () {
+     $("#Clifton-Beach").click(function () {
         window.location.href = "sindh-description/Clifton-Beach.html";
     });
 
-     $("#Hawksbay Beach").click(function () {
+     $("#Hawksbay-Beach").click(function () {
         window.location.href = "sindh-description/Hawksbay-Beach.html";
     });
 
-     $("#Mohatta Palace").click(function () {
+     $("#Mohatta-Palace").click(function () {
         window.location.href = "sindh-description/Mohatta-Palace.html";
     });
 });
