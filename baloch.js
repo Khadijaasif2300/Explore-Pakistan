8219a3116802d5739c3e134aa78286a0128b3740
khadijaasif2300 <khadijaasif230@gmail.com>
2025-08-27 20:28:47 +0500

    addition

--- baloch.js
+++ baloch.js
@@ -1,7 +1,69 @@
 $(document).ready(function(){
-    $(".footer").load("footer.html");
-});
-
-$(document).ready(function(){
     $(".header").load("header.html");
+
+    $(".footer").load("footer.html");
+
+    $("#Quetta City").click(function () {
+        window.location.href = "sindh-description/Quetta City.html";
+    });
+
+    $("#Hanna Lake").click(function () {
+        window.location.href = "sindh-description/Hanna Lake.html";
+    });
+
+    $("#Urak Valley").click(function () {
+        window.location.href = "sindh-description/Urak Valley.html";
+    });
+
+    $("#Ziarat").click(function () {
+        window.location.href = "sindh-description/Ziarat.html";
+    });
+
+    $("#Quaid Residency").click(function () {
+        window.location.href = "sindh-description/Quaid Residency.html";
+    });
+
+    $("#Ziarat Juniper Forest").click(function () {
+        window.location.href = "sindh-description/Ziarat Juniper Forest.html";
+    });
+
+    $("#Kund Malir Beach").click(function () {
+        window.location.href = "sindh-description/Kund Malir Beach.html";
+    });
+
+    $("#Hingol National Park").click(function () {
+        window.location.href = "sindh-description/Hingol National Park.html";
+    });
+
+    $("#Princess of Hope").click(function () {
+        window.location.href = "sindh-description/Princess of Hope.html";
+    });
+
+    $("#Mud Volcanoes").click(function () {
+        window.location.href = "sindh-description/Mud Volcanoes.html";
+    });
+
+    $("#Pir Ghaib Waterfalls").click(function () {
+        window.location.href = "sindh-description/Pir Ghaib Waterfalls.html";
+    });
+
+     $("#Bolan Pass").click(function () {
+        window.location.href = "sindh-description/Bolan Pass.html";
+    });
+
+     $("#Moola Chotok").click(function () {
+        window.location.href = "sindh-description/Moola Chotok.html";
+    });
+
+     $("#Astola Island").click(function () {
+        window.location.href = "sindh-description/Astola Island.html";
+    });
+
+     $("#Gwadar Port & Hammerhead").click(function () {
+        window.location.href = "sindh-description/Gwadar Port .html";
+    });
+
+    $("#Pasni Beach").click(function () {
+        window.location.href = "sindh-description/Pasni Beach .html";
+    });
 });
--- kpk.js
+++ kpk.js
@@ -1,7 +1,69 @@
 $(document).ready(function(){
-    $(".footer").load("footer.html");
-});
-
-$(document).ready(function(){
     $(".header").load("header.html");
+
+    $(".footer").load("footer.html");
+
+    $("#Peshawar City").click(function () {
+        window.location.href = "sindh-description/Peshawar City.html";
+    });
+
+    $("#Islamia College").click(function () {
+        window.location.href = "sindh-description/Islamia College.html";
+    });
+
+    $("#Bala Hisar Fort").click(function () {
+        window.location.href = "sindh-description/Bala Hisar Fort.html";
+    });
+
+    $("#Mahabat Khan Mosque").click(function () {
+        window.location.href = "sindh-description/Mahabat Khan Mosque.html";
+    });
+
+    $("#Khyber Pass").click(function () {
+        window.location.href = "sindh-description/Khyber Pass.html";
+    });
+
+    $("#Swat Valley").click(function () {
+        window.location.href = "sindh-description/Swat Valley.html";
+    });
+
+    $("#Malam Jabba").click(function () {
+        window.location.href = "sindh-description/Malam Jabba.html";
+    });
+
+    $("#Kalam Valley").click(function () {
+        window.location.href = "sindh-description/Kalam Valley.html";
+    });
+
+    $("#Mingora").click(function () {
+        window.location.href = "sindh-description/Mingora.html";
+    });
+
+    $("#Kumrat Valley").click(function () {
+        window.location.href = "sindh-description/Kumrat Valley.html";
+    });
+
+    $("#Shandur Pass").click(function () {
+        window.location.href = "sindh-description/Shandur Pass.html";
+    });
+
+     $("#Chitral Valley").click(function () {
+        window.location.href = "sindh-description/Chitral Valley.html";
+    });
+
+     $("#Kalash Valley").click(function () {
+        window.location.href = "sindh-description/Kalash Valley.html";
+    });
+
+     $("#Fairy Meadows").click(function () {
+        window.location.href = "sindh-description/Fairy Meadows.html";
+    });
+
+     $("#Saiful Muluk Lake").click(function () {
+        window.location.href = "sindh-description/Saiful Muluk Lake.html";
+    });
+
+    $("#Babusar Top").click(function () {
+        window.location.href = "sindh-description/Babusar Top.html";
+    });
 });
